15a88e7f2eefaa88ffd5af27ac077f01842903d5
KilFex <elivan.1251996@gmail.com>
2025-04-27 00:47:13 -0500

    Ya estamos dando nuestos primeros ejercicios, lo bueno es q no truena si me olvido el ; xddd

---
+++ Ejercicio/ejercicio_01/script.js
@@ -0,0 +1,54 @@
+/* Ejercicio:
+
+Tienes un array de objetos llamado productos. Cada objeto representa un producto con las siguientes propiedades: 
+nombre (string), precio (número) y disponible (boolean).
+
+Debes hacer lo siguiente:
+
+Recorrer el array con un bucle.
+
+Para cada producto:
+
+Si disponible es true y el precio es menor o igual a 100, mostrar en consola:
+"Producto disponible: [nombre] - Precio: [precio]".
+
+Si el producto no está disponible, mostrar:
+"Producto no disponible: [nombre]".
+
+Al final, fuera del bucle, muestra un listado nuevo (otro array) que contenga solo los nombres 
+(nombre) de los productos que estaban disponibles y cuyo precio era menor o igual a 100.*/
+
+
+const productos = [
+    {nombre: "Camiseta", precio: 50, disponible: false},
+    {nombre: "pantalon", precio: 150, disponible: false},
+    {nombre: "zapatos", precio: 80, disponible: true},
+    {nombre: "gorra", precio:30, disponible: true},
+    {nombre: "reloj", precio:200, disponible: false}
+];
+
+const productosDisponibles = [];
+/*
+for(let i = 0; i < productos.length; i++){
+    const producto = productos[i]
+
+    if(producto.disponible & producto.precio <= 100){
+        console.log(`Producto disponible: ${producto.nombre} - Precio: ${producto.precio}`)
+        productosDisponibles.push(producto.nombre)
+    }else if(!producto.disponible){
+        console.log(`Producto no disponible: ${producto.nombre}`)
+    }
+}
+
+console.log(`Productos disponibles a buen preio: ${productosDisponibles}`);*/
+
+productos.forEach((producto) => {
+    if(producto.disponible & producto.precio <= 100){
+        console.log(`Producto disponible: ${producto.nombre} - Precio: ${producto.precio}`)
+        productosDisponibles.push(producto.nombre)
+    }else if(!producto.disponible){
+        console.log(`Producto no disponible: ${producto.nombre}`)
+    }
+});
+
+console.log(`Productos disponibles a buen preio: ${productosDisponibles}`);
---
+++ Ejercicio/ejercicio_02/script.js
@@ -0,0 +1,55 @@
+/*Tienes el siguiente array de productos (son objetos con nombre,
+ precio, disponible y ahora también una nueva propiedad: categoria).
+
+Debes hacer lo siguiente:
+
+Recorrer todo el array.
+
+Para cada producto:
+
+Si el producto es disponible y su precio es menor o igual a 100, muestra en consola:
+"Producto económico disponible: [nombre] - Categoría: [categoria]".
+
+Si el producto no está disponible, muestra:
+"Producto agotado: [nombre] - Categoría: [categoria]".
+
+Si el producto es disponible pero su precio es mayor a 100, muestra:
+"Producto premium disponible: [nombre] - Precio: [precio]".
+
+Al final:
+
+Crea dos nuevos arrays:
+
+Uno con los nombres de todos los productos económicos disponibles (precio <= 100).
+
+Otro con los nombres de todos los productos premium disponibles (precio > 100). */
+
+const productos = [
+    {nombre: "Camiseta", precio: 50, categoria:"Ropa" , disponible:true},
+    {nombre: "Pantalon", precio: 120, categoria: "Ropa" , disponible:false},
+    {nombre: "Zapatos", precio: 80, categoria:"Calzado" , disponible: true},
+    {nombre: "Gorra", precio: 30, categoria: "Accesorios" , disponible: true},
+    {nombre: "Reloj", precio: 200, categoria: "Accesorios" , disponible: false},
+    {nombre: "Mochila", precio: 90, categoria: "Accesorios" , disponible: true},
+]
+
+const productosEconomicosDisponibles = [],
+        productosPremiunDisponibles = []
+
+productos.forEach((producto) => {
+    if(producto.disponible & producto.precio <= 100){
+        console.log(`Producto economico disopnible: ${producto.nombre} - 
+            categoria: ${producto.categoria}`)
+            productosEconomicosDisponibles.push(producto.nombre)
+    } else if(!producto.disponible){
+        console.log(`Producto agotado: ${producto.nombre} - Categoria: ${producto.categoria}`)
+    } else if(producto.disponible & producto.precio > 100){
+        console.log(`Producto premiun disponible: ${producto.nombre} - Precio: ${producto.precio}`)
+        productosPremiunDisponibles.push(producto.nombre)
+    }
+
+});
+
+console.log(`Productos economicos disponibles: ${productosEconomicosDisponibles}`)
+console.log(`Productos premiun disponibles: ${productosPremiunDisponibles}`)
+
